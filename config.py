bc5658c44a73cd37534517aedd740ce514bb9030
Leader0Z <159470878+Leader0Z@users.noreply.github.com>
2024-02-15 20:36:21 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ r = redis.Redis(
   port=17811,
   password='y41sFD7N8cY5Ob2MGPZkGdrTndVFY92h')
 
-sudo_id = 6581896306
+sudo_id = 6766128426
 bot_user = Mody.BOT_USER
 via_user = Mody.VIA_USER
 elhyba = bot_user
@@ -16,9 +16,9 @@ api_id = Mody.APP_ID
 api_hash = Mody.API_HASH
 session = Mody.SESSION
 token = Mody.TG_BOT_TOKEN
-sudo_command = [6581896306]
+sudo_command = [6766128426]
 pm =  Mody.MENTION
-mention = "6581896306"
+mention = "6766128426"
 plugins = dict(root="plugins")
 app = Client(via,api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
 bot = Client(elhyba,api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
